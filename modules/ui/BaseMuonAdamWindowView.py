798ffc50dfbcad3890685a78e133054617044fd8
AmaelG <amael@gabory.org>
2026-06-01 14:16:11 +0200

    merge anima and adv_optm

--- modules/ui/BaseMuonAdamWindowView.py
+++ modules/ui/BaseMuonAdamWindowView.py
@@ -43,9 +43,9 @@ class BaseMuonAdamWindowView:
 
             self.components.label(master, row, col, title, tooltip=tooltip)
 
-            if param_type != 'bool':
+            if param_type == 'StatePrecision':
+                self.components.options(master, row, col + 1, ["auto", "factored", "fp32", "bf16_sr", "int8_sr"], ui_state, key)
+            elif param_type != 'bool':
                 self.components.entry(master, row, col + 1, ui_state, key)
-            elif param_type == 'StatePrecision':
-                self.components.options(master, row, col + 1, ["auto", "factored", "fp32", "bf16_sr", "int8_sr"], self.adam_ui_state, key)
             else:
                 self.components.switch(master, row, col + 1, ui_state, key)
